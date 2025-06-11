ec94a3cb6d819e6fbea358ea6ee518f897145ebe
gerard <12766134+scripting-drafts@users.noreply.github.com>
2025-06-11 14:36:21 +0200

    NLP vocab tool, letter:score values

--- NLP/drafts/scrabble_test.py
+++ NLP/drafts/scrabble_test.py
@@ -1,7 +1,7 @@
 import itertools
 
 letter_scores = {
-    'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+    'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'Ñ': 1, 'R': 1, 'S': 1, 'T': 1,
     'D': 2, 'G': 2,
     'B': 3, 'C': 3, 'M': 3, 'P': 3,
     'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
@@ -16,7 +16,8 @@ def scrabble_score(word):
         total_score += letter_scores.get(letter, 0) # Use .get() to handle unknown letters
     return total_score
 
-letters = ['A', 'D', 'F', 'B', 'E', 'E', 'U']
+letters = ['Ñ', 'U', 'F', 'B', 'E', 'E', 'U']
+letters = [letter.lower() for letter in letters]
 
 vocab = open('../resources/wordlist.txt', 'r').read().split()
 
@@ -35,7 +36,12 @@ while True:
 valuable_combinations = []
 
 for com in combinations:
+    com = ''.join(com)
     if com in vocab:
         valuable_combinations.append(com)
 
 print(valuable_combinations)
+word_values = [(com, sum([letter_scores[letter] for letter in com.upper()])) for com in valuable_combinations]
+
+for value in word_values:
+    print(f'{value[0]}: {value[1]}')
